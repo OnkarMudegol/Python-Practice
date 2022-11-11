897b34b7aa91110447d19af5b584ffd743263bfa
OnkarMudegol <omudegol@gmail.com>
2022-11-11 23:32:41 +0530

    Number pyramid

--- pyramidType.py
+++ pyramidType.py
@@ -17,7 +17,7 @@ for i in range(rows):
 rows = int(input("Enter number of rows: "))
 
 for i in range(rows, 0, -1):
-    for j in range(1, i+1):
+    for j in range(i,0,-1):
         print(j, end=" ")
     
     print("\n")
